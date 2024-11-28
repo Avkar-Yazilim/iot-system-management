d28f1deb97b74671b6f6e9fd4eaede7e72dafeca
yigit.ture <yigit.ture@avkaryazilim.com.tr>
2024-11-29 00:03:49 +0300

    project init.

---
+++ backend/src/main/java/tr/com/targe/iot/IOTSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package tr.com.targe.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IOTSystemManagementApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(IOTSystemManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tr/com/targe/iot/IOTSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.targe.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOTSystemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
